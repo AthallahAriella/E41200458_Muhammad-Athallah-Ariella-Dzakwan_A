c9dd20b23fa55ee0aab4435bcb089ef46b0109fd
Muhammad Athallah Ariella Dzakwan <80690772+E41200458-Muhammad-Athallah-Ariella@users.noreply.github.com>
2021-04-11 01:21:58 +0700

    week 4

---
+++ tugas_w04/src/tugas_w04/tugas2.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas_w04;
+
+/**
+ *
+ * @author Athallah Ariella
+ */
+public class tugas2 {
+    
+     public static void main(String[] args) {
+         
+         int number = 1;
+         
+         System.out.println("         Do_while            ");
+         System.out.println("Bilangan Kelipatan 2 (1-100) ");
+         System.out.println("=============================");
+         int i = 1;
+         do {
+             System.out.print(i*2 + " ");
+             i++;
+         }
+         while (i<=50);
+         System.out.println(" ");
+         System.out.println(" ");
+         
+         System.out.println(" Perkalian 2 (1-100) ");
+         System.out.println("=====================");
+         System.out.print("   " + number + " ");
+         do {
+             number *= 2;
+             if (number > 100){
+                 break;
+             }
+             System.out.print(number + " ");
+         } while (number < 100 );
+         System.out.println("");
+     }
+    
+}
