cc5c6ed6aa3c7f1105e4a138ecb7f74e770402aa
Muhammad Athallah Ariella Dzakwan <80690772+E41200458-Muhammad-Athallah-Ariella@users.noreply.github.com>
2021-04-25 13:59:22 +0700

    week 07

---
+++ week_07/src/Pewarisan/Test.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pewarisan;
+
+/**
+ *
+ * @author Athallah
+ */
+public class Test {
+    public static void main(String[] args) {
+        Pelajar mahasiswa = new Pelajar("Muhammad Athallah Ariella Dzakwna", 151, 43, "E41200458", "POLITEKNIK NEGERI JEMBER", 85);
+        System.out.println(mahasiswa.toString());
+    }
+}
