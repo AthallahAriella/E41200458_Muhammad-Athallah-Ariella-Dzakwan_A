6dda9d41a51c20b86edfe92a87cdae897e5a81e1
Muhammad Athallah Ariella Dzakwan <80690772+E41200458-Muhammad-Athallah-Ariella@users.noreply.github.com>
2021-07-11 21:15:54 +0700

    week_5
    week_5

---
+++ week_05/src/week_05/vertex.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week_05;
+
+/**
+ *
+ * @author Athallah Ariella
+ */
+public class vertex {
+    public char label;
+    public boolean wasVisited;
+    
+    public vertex(char label) {
+        this.label = label; wasVisited = false;
+        
+    }
+}
